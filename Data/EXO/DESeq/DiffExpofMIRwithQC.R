c35ed45c6cae8bb0b115a8647a3b52b92b125738
Harley.Robinson@uqconnect.edu.au <hrobinson@di.uq.edu.au>
2016-02-11 15:42:25 +1000

    11/2/16 background, info, qc for sub data

--- Data/EXO/DESeq/DiffExpofMIRwithQC.R
+++ Data/EXO/DESeq/DiffExpofMIRwithQC.R
@@ -1,19 +1,19 @@
 library(DESeq2)
 #Always filter and normalise the data first (FilteringandSubsetting.r)
-rawdata<- read.delim(file="HEKexoFiltered.txt", header= TRUE, row.names=1)
+rawdata<- read.csv(file="miRNA_analysis_kerry.csv", header= TRUE, row.names=1)
 Cav2PN4= rawdata[c(13:15, 1:3)]
-Cav1PN4= rawdata[c(10:12, 1:3)]
+Cav1PN4= rawdata[c(1:3, 5, 6, 7)]
 CaveolinPN4= rawdata[c(1:3, 4:6)]
 Cav3PN4= rawdata[c(7:9, 1:3)]
 
 #Analysis for Cavin
-exp_designCav = data.frame(row.names = colnames(Cav3PN4),
+exp_designCav = data.frame(row.names = colnames(Cav1PN4),
                             condition =c("Test", "Test", "Test", "Cont", "Cont", "Cont"),
                             libType = c("paired-end", "paired-end", "paired-end", "paired-end", "paired-end", "paired-end"))
 condition= relevel(exp_designCav$condition, "Test")
 levels(exp_designCav)
 head(exp_designCav)
-ddsCav=DESeqDataSetFromMatrix(countData=Cav3PN4, colData=exp_designCav, design=~condition)
+ddsCav=DESeqDataSetFromMatrix(countData=Cav1PN4, colData=exp_designCav, design=~condition)
 ddsCav=DESeq(ddsCav)
 resCav= results(ddsCav, alpha=0.05)
 write.csv(resCav, "HEKEXOCaveVCav3.csv")
@@ -21,6 +21,7 @@ write.csv(resCav, "HEKEXOCaveVCav3.csv")
 rlogoddsdds=rlogTransformation(ddsCav1)
 png("plotPCA_Test.png")
 plotPCA(rlogoddsdds)
+text(rownames(rlogoddsdds), pos= 3)
 dev.off()
 
 #graph below gives best mirna given high BHpval annnnd pvalue. 
--- Data/EXO/EDGER/edgeR analysis of data.r
+++ Data/EXO/EDGER/edgeR analysis of data.r
@@ -5,7 +5,7 @@ library(lumi)
 # read in the raw data
 rawdata <- read.delim("miRNA_raw_counts_CORRECT_NAMES.txt")
 # create a DGEList object. In this case, raw data is in columns 2 to 55, and unique identifiers are in 1 (miRNA name)
-y <- DGEList(counts=rawdata[,1:8], genes=rownames(rawdata))
+y <- DGEList(counts=Cav1PN4[,1:6], genes=rownames(rawdata))
 # Filtering and Normalization
 # compute the effective library size by using TMM normalization
 keep <- rowSums(cpm(y)>10) >=2
@@ -20,7 +20,7 @@ dist<-dist(d)
 hc<-hclust(dist)
 plot(hc)
 # Export the graph to a PDF
-pdf(file="dendrogram.pdf", paper="a4r")
+pdf(file="dendrogramPC3Cav1Sub.pdf", paper="a4r")
 plot(hc)
 dev.off()
 
@@ -34,7 +34,7 @@ legend("topright", legend = c("Exosome", "Pellet"), col = 1:2, pch = 15)
 plotMDS(d, col=c(rep(1,30), rep(2,24))) 
 legend("topright", legend = c("HEK", "PC3"), col = 1:2, pch = 15) 
 # Export the graph to a PDF
-pdf(file="MDS.pdf", paper="a4r")
+pdf(file="MDSCavin1withSubPC3exo.pdf", paper="a4r")
 plotMDS(d, col=c(rep(1,15), rep(2, 15), rep(1, 12), rep(2, 12))) 
 legend("topright", legend = c("Exosome", "Pellet"), col = 1:2, pch = 15) 
 
