b2a422c01ff7e809e162643f3b9a2dfb0edb325d
Harley.Robinson@uqconnect.edu.au <hrobinson@di.uq.edu.au>
2016-04-11 14:44:50 +1000

    11/4/16

---
+++ Data/Lipid raft/PWMtoscore.r
@@ -0,0 +1,18 @@
+
+pwm<- read.csv("FUSpwmGGUG.csv", header= TRUE, row.names=1)
+seq<- read.csv("seqs.csv", header=FALSE, row.name=1)
+n=1
+windowscore<-0
+for (n in 1:(length(seq)-4)) {
+  window<- seq[9, n:(n+3)]
+  i=1
+  d=0
+  for (column in window) {
+    d[i]<-pwm[as.character(column), i]
+    i=i+1
+  }
+  windowscore[n]<- d[1]*d[2]*d[3]*d[4]
+  n=n+1
+}
+windowscore
+max(windowscore)
