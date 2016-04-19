bb54f4b02b5c40b28605f70002c5e27305f17f2f
Harley.Robinson@uqconnect.edu.au <hrobinson@di.uq.edu.au>
2016-04-19 15:08:04 +1000

    19/4/16

--- Data/Lipid raft/PWMtoscore.r
+++ Data/Lipid raft/PWMtoscore.r
@@ -1,17 +1,17 @@
 
-pwm<- read.csv("FUSpwmGGUG.csv", header= TRUE, row.names=1)
-seq<- read.csv("seqs.csv", header=FALSE, row.name=1)
+pwm<- read.csv("MotiffromDiffDownWithpseudo.csv", header= TRUE, row.names=1)
+seq<- read.csv("seqs.csv", header=FALSE, row.names=1)
 n=1
 windowscore<-0
-for (n in 1:(length(seq)-4)) {
-  window<- seq[9, n:(n+3)]
+for (n in 1:(length(seq)-5)) {
+  window<- seq[4, n:(n+4)]
   i=1
   d=0
   for (column in window) {
     d[i]<-pwm[as.character(column), i]
     i=i+1
   }
-  windowscore[n]<- d[1]*d[2]*d[3]*d[4]
+  windowscore[n]<- d[1]*d[2]*d[3]*d[4]*d[5]
   n=n+1
 }
 windowscore
