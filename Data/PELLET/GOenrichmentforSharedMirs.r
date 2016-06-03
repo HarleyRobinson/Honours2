1fbde4e98b2440f7ded2c5065942a9199e95ecb3
Harley.Robinson@uqconnect.edu.au <hrobinson@di.uq.edu.au>
2016-06-03 15:18:51 +1000

    3/6/15

--- Data/PELLET/GOenrichmentforSharedMirs.r
+++ Data/PELLET/GOenrichmentforSharedMirs.r
@@ -1,17 +1,18 @@
 library(topGO)
 #using files generated from ComparingPredicted...
-data=read.csv("OrderedmiR363proteins.csv", header=TRUE)
+data=read.csv("PartnersofInterestingMirs.csv", header=FALSE)
 #Sorting for only genes that are expected to be changed in expression
 #For Upreg miR, protein should be negative and vice versa. 
-keep<- data[data$log2FoldChange>=0, ]
+keep<- data[1:819, ]
 #write.csv(keep, "PC3PelletmiR20bfiltered.csv")
 #next part needs annotate. 
-all<- factor(as.integer(data[, 3]%in%keep[, 3]))
-names(all)<- data[, 3]
+
+all<- factor(as.integer(data[, 1]%in%keep[, 1]))
+names(all)<- data[, 1]
 selection= function(x) TRUE
 GOdata<- new("topGOdata", ontology="BP", allGenes= all, geneSel=function(p) p==1, description= "miR targets", annot=annFUN.org, mapping="org.Hs.eg.db", ID= "symbol")
 results.ks= runTest(GOdata, algorithm = "classic", statistic = "ks")
 results.ks
 allRes= GenTable(GOdata, KS= results.ks, orderBy= 'KS', topNodes=25)
 allRes[,c('GO.ID', 'Term', 'KS')]
-write.csv(allRes[,c('GO.ID', 'Term', 'KS')], "GOenrichmentMIR363.csv")
+write.csv(allRes[,c('GO.ID', 'Term', 'KS')], "InterestingMirsenrichment.csv")
