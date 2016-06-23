47a69f56b20c19829c2cc614547e08c992471833
Harley.Robinson@uqconnect.edu.au <hrobinson@di.uq.edu.au>
2016-06-23 14:09:45 +1000

    23/6/16

---
+++ Data/Motif stuff/HeatMapGenerator.r
@@ -0,0 +1,35 @@
+if (!require("gplots")) {
+  install.packages("gplots", dependencies = TRUE)
+  library(gplots)
+}
+if (!require("RColorBrewer")) {
+  install.packages("RColorBrewer", dependencies = TRUE)
+  library(RColorBrewer)
+}
+data <- read.csv("HeatMapTest.csv", comment.char="#")
+rnames <- data[,1]                            # assign labels in column 1 to "rnames"
+mat_data <- data.matrix(data[,2:ncol(data)])  # transform column 2-5 into a matrix
+rownames(mat_data) <- rnames
+my_palette <- colorRampPalette(c("red", "yellow", "green"))(n = 299)
+col_breaks = c(seq(-1,0.0001,length=100),  # for red
+               seq(0.0002,0.8,length=100),              # for yellow
+               seq(0.80001,1,length=100))              # for green
+png("Maps.png",    # create PNG for the heat map        
+    width = 5*300,        # 5 x 300 pixels
+    height = 5*300,
+    res = 300,            # 300 pixels per inch
+    pointsize = 8)        # smaller font size
+
+heatmap.2(mat_data,
+          cellnote = mat_data,  # same data set for cell labels
+          main = "Correlation", # heat map title
+          notecol="black",      # change font color of cell labels to black
+          density.info="none",  # turns off density plot inside color legend
+          trace="none",         # turns off trace lines inside the heat map
+          margins =c(12,9),     # widens margins around plot
+          col=my_palette,       # use on color palette defined earlier
+          breaks=col_breaks,    # enable color transition at specified limits
+          dendrogram="row",     # only draw a row dendrogram
+          Colv="NA")            # turn off column clustering
+
+dev.off()               # close the PNG device
