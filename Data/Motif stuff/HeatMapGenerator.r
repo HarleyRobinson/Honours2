cdc6b2300268df4c429519037aaba707a9a159a2
Harley.Robinson@uqconnect.edu.au <hrobinson@di.uq.edu.au>
2016-06-29 16:08:44 +1000

    29/6/16

--- Data/Motif stuff/HeatMapGenerator.r
+++ Data/Motif stuff/HeatMapGenerator.r
@@ -6,15 +6,15 @@ if (!require("RColorBrewer")) {
   install.packages("RColorBrewer", dependencies = TRUE)
   library(RColorBrewer)
 }
-data <- read.csv("HeatMapTest.csv", comment.char="#")
+data <- read.csv("HeatMapDct.csv", comment.char="#")
 rnames <- data[,1]                            # assign labels in column 1 to "rnames"
 mat_data <- data.matrix(data[,2:ncol(data)])  # transform column 2-5 into a matrix
 rownames(mat_data) <- rnames
 my_palette <- colorRampPalette(c("black", "grey", "white"))(n = 299)
-col_breaks = c(seq(-10,-2,length=100),  # for red
-               seq(-2.1, 2,length=100),              # for yellow
-               seq(2.1, 10,length=100))              # for green
-png("Maps.png",    # create PNG for the heat map        
+col_breaks = c(seq(-7,-1,length=100),  # for red
+               seq(-1.1, 1,length=100),              # for yellow
+               seq(1.1, 7,length=100))              # for green
+png("NewMapsDct.png",    # create PNG for the heat map        
     width = 5*300,        # 5 x 300 pixels
     height = 5*300,
     res = 300,            # 300 pixels per inch
@@ -22,14 +22,14 @@ png("Maps.png",    # create PNG for the heat map
 
 heatmap.2(mat_data,
           cellnote = mat_data,  # same data set for cell labels
-          main = "Correlation", # heat map title
-          notecol="black",      # change font color of cell labels to black
+          main = "Dct expression based on location", # heat map title
+          notecol="none",      # change font color of cell labels to black
           density.info="none",  # turns off density plot inside color legend
           trace="none",         # turns off trace lines inside the heat map
-          margins =c(12,9),     # widens margins around plot
+          margins =c(9,9),     # widens margins around plot
           col=my_palette,       # use on color palette defined earlier
           breaks=col_breaks,    # enable color transition at specified limits
-          dendrogram="row",     # only draw a row dendrogram
+          dendrogram="none",     # only draw a row dendrogram
           Colv="NA")            # turn off column clustering
 
 dev.off()               # close the PNG device
